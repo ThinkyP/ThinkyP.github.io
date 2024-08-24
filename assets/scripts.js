d63d818614718ec3230046d5a7ae68f0e43a5841
Pedro Treuer <treuerpedro@gmail.com>
2024-08-24 12:58:25 +0200

    t

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,14 @@
 // This is a placeholder for your JavaScript. 
 // You can add interactivity here if needed, such as toggling sections, fetching posts dynamically, etc.
 
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Website loaded successfully.');
-});
-
-// header.js
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('../header.html')
+function loadHeader() {
+    fetch('header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header-placeholder').innerHTML = data;
         });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loadHeader();
 });
